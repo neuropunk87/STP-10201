d374596df1f5ad01b41139a4eeec6ab00ba7c064
MaksymKorotchenko <maximuskor29@gmail.com>
2025-08-26 18:11:23 +0300

    gallery section done

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,20 +1,30 @@
 const openBtnEl = document.querySelector('[data-action="open"]');
 const closeBtnEl = document.querySelector('[data-action="close"]');
-const burgerMenuEl = document.querySelector('[data-visible]');
-
-openBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'open';
-  handleModal();
-});
-
-closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.dataset.visible = 'close';
-  handleModal();
-});
+const burgerMenuEl = document.querySelector('.js-header-modal');
 
 function handleModal() {
-  const isOpen = burgerMenuEl.dataset.visible === 'open';
+  const isOpen = burgerMenuEl.classList.contains('is-open');
   openBtnEl.style.display = isOpen ? 'none' : 'block';
   closeBtnEl.style.display = isOpen ? 'flex' : 'none';
   document.body.style.overflow = isOpen ? 'hidden' : 'auto';
 }
+
+openBtnEl.addEventListener('click', e => {
+  burgerMenuEl.classList.replace('is-closed', 'is-open');
+  handleModal();
+});
+
+closeBtnEl.addEventListener('click', e => {
+  burgerMenuEl.classList.replace('is-open', 'is-closed');
+  handleModal();
+});
+
+function closeModal() {
+  burgerMenuEl.addEventListener('click', event => {
+    if (event.target.tagName === 'A') {
+      burgerMenuEl.classList.replace('is-open', 'is-closed');
+      handleModal();
+    }
+  });
+}
+closeModal();
