85713d9d8f2f31541e87b40301a6218822ffd668
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-08-26 21:27:22 +0300

    Added Gallery section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,1 +1,23 @@
-// swiper
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+  modules: [Navigation],
+  slidesPerView: 2,
+  spaceBetween: 11,
+  navigation: {
+    nextEl: '.gallery-swiper-btn-next',
+    prevEl: '.gallery-swiper-btn-prev',
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+  loop: false,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  watchOverflow: true,
+});
