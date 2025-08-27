f41d4c61156f89eede9bfcb30864fe1e177919ee
MaksymKorotchenko <maximuskor29@gmail.com>
2025-08-27 22:23:31 +0300

    minor fixes

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -3,26 +3,26 @@ const closeBtnEl = document.querySelector('[data-action="close"]');
 const burgerMenuEl = document.querySelector('.js-header-modal');
 
 function handleModal() {
-  const isOpen = burgerMenuEl.classList.contains('is-open');
+  const isOpen = burgerMenuEl.classList.contains('js-is-open');
   openBtnEl.style.display = isOpen ? 'none' : 'block';
   closeBtnEl.style.display = isOpen ? 'flex' : 'none';
   document.body.style.overflow = isOpen ? 'hidden' : 'auto';
 }
 
 openBtnEl.addEventListener('click', e => {
-  burgerMenuEl.classList.replace('is-closed', 'is-open');
+  burgerMenuEl.classList.replace('js-is-closed', 'js-is-open');
   handleModal();
 });
 
 closeBtnEl.addEventListener('click', e => {
-  burgerMenuEl.classList.replace('is-open', 'is-closed');
+  burgerMenuEl.classList.replace('js-is-open', 'js-is-closed');
   handleModal();
 });
 
 function closeModal() {
   burgerMenuEl.addEventListener('click', event => {
     if (event.target.tagName === 'A') {
-      burgerMenuEl.classList.replace('is-open', 'is-closed');
+      burgerMenuEl.classList.replace('js-is-open', 'js-is-closed');
       handleModal();
     }
   });
